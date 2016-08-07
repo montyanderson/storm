424ee947262dde689391825b8231449981002070
Monty Anderson <monty.linux@gmail.com>
2016-08-07 23:33:19 +0100

    more modular!

---
+++ routes/search.js
@@ -0,0 +1,42 @@
+const querystring = require("querystring");
+const got = require("got");
+const db = require("../lib/db.js");
+
+module.exports = (req, res) => {
+	const q = req.query.q;
+
+	db.get("search:" + q, (err, data) => {
+		if(data) return res.end(data);
+
+		const client_id = "f946296624e1496f9a1f6310973c744b";
+
+		got("https://api.spotify.com/v1/search?" + querystring.stringify({
+			client_id, q,
+			type: "artist"
+		})).then((response) => {
+			const artists = JSON.parse(response.body).artists.items.map((a) => {
+				const b = {
+					name: a.name,
+					id: a.id,
+					genres: a.genres
+				};
+
+				if(a.images[0]) b.image = a.images[0].url;
+
+				return b;
+			});
+
+			const data = JSON.stringify(artists);
+
+			db.multi()
+				.set("search:" + q, data)
+				.expire("search:" + q, 60 * 60 * 1)
+				.exec(() => {
+						res.end(data);
+			});
+		}).catch((error) => {
+			console.log(error);
+			res.end();
+		});
+	});
+};
