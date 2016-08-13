af3e0a21833c7c7f0b1f07ed38f779ccaf3294ca
Monty Anderson <monty.linux@gmail.com>
2016-08-13 19:36:15 +0100

    works with tags too!

--- routes/generate.js
+++ routes/generate.js
@@ -13,10 +13,10 @@ module.exports = [
 			res.locals.cards = [];
 		}
 
-		res.locals.songs = [];
+		res.locals.tracks = [];
 		next();
 	},
-	(req, res, next) => { /* get songs for artists from spotify */
+	(req, res, next) => { /* get tracks for artists from spotify */
 		async.map(res.locals.cards.filter(a => a.type == "artist"), (artist, callback) => {
 			got("https://api.spotify.com/v1/artists/" + artist.id + "/top-tracks?country=GB").then((response) => {
 				const data = JSON.parse(response.body);
@@ -25,17 +25,39 @@ module.exports = [
 			}).catch(callback);
 		}, (err, artist) => {
 			artist.forEach((artist) => {
-				artist.forEach(song => res.locals.songs.push(song));
+				artist.forEach(song => res.locals.tracks.push(song));
 			});
 
 			next();
 		});
 	},
-	(req, res, next) => {
-		shuffle(res.locals.songs);
+	(req, res, next) => { /* gets top tracks for a tag then search for them on spotify */
+		async.map(res.locals.cards.filter(a => a.type == "tag"), (tag, callback) => {
+			got("https://ws.audioscrobbler.com/2.0/?" + querystring.stringify({
+				api_key: config.lastfm,
+				tag: tag.id,
+				method: "tag.gettoptracks",
+				format: "json"
+			})).then((response) => {
+				async.map(JSON.parse(response.body).tracks.track.slice(0, 6), (track, callback) => {
+					got("https://api.spotify.com/v1/search?" + querystring.stringify({
+						q: track.name + " " + track.artist.name,
+						type: "track"
+					})).then((response) => {
+						callback(null, JSON.parse(response.body).tracks.items[0].id);
+					}).catch(callback);
+				}, callback);
+			}).catch(callback);
+		}, (err, tags) => {
+			tags.forEach(tag => res.locals.tracks.push(...tag));
+			next();
+		});
+	},
+	(req, res, next) => { /* randomly shuffle array */
+		shuffle(res.locals.tracks);
 		next();
 	},
-	(req, res, next) => {
-		res.end(JSON.stringify(res.locals.songs));
+	(req, res, next) => { /* serve tracks */
+		res.end(JSON.stringify(res.locals.tracks));
 	}
 ];
