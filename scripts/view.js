d32ba4e74ee2440f4e3be91b4a77c3e5fd322f6a
Monty Anderson <monty.linux@gmail.com>
2016-08-16 15:30:39 +0100

    view.js now uses es6 classes

--- scripts/view.js
+++ scripts/view.js
@@ -1,11 +1,14 @@
 const Hogan = require("hogan.js");
 
-const View = module.exports = function(template, init) {
-	this.template = Hogan.compile(template);
-	this.init = init || function() {};
-	this.locals = {};
-}
+module.exports = class View {
+	constructor(template, init) {
+		this.template = Hogan.compile(template);
+		this.init = init || function() {};
+		this.locals = {};
+	}
 
-View.prototype.render = function(locals) {
-	this.init(this.template.render(Object.assign({}, this.locals, locals)));
+
+	render(locals) {
+		this.init(this.template.render(Object.assign({}, this.locals, locals)));
+	}
 }
